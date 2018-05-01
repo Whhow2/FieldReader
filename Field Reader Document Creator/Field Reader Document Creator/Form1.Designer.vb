ab89491cb4652e95a977f0e372c9033da961bb51
whhow2 <whhow2@gmail.com>
2018-04-30 22:47:30 -0700

    master files

---
+++ Field Reader Document Creator/Field Reader Document Creator/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Selected Documents"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 163)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Destination Folder"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label3.Location = New System.Drawing.Point(12, 181)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(449, 23)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Please select a folder to save converted files."
+        '
+        'ListBox1
+        '
+        Me.ListBox1.AllowDrop = True
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.Location = New System.Drawing.Point(15, 25)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.ListBox1.Size = New System.Drawing.Size(446, 121)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(467, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(467, 54)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Remove"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(467, 181)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Browse"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Multiselect = True
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(175, 212)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Convert"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 241)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(446, 12)
+        Me.ProgressBar1.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(467, 241)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Not Started"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(256, 212)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "Quit"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Enabled = False
+        Me.Button6.Location = New System.Drawing.Point(467, 87)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 12
+        Me.Button6.Text = "Clear"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(556, 275)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Field Reader Document Creator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+End Class
---
+++ Field Reader Document Creator/Field Reader Document Creator/Form1.vb
@@ -0,0 +1,200 @@
+﻿Imports System.IO
+Imports Word = Microsoft.Office.Interop.Word
+
+Public Class Form1
+    Dim SelectedFiles() As String
+    Public Sub InitializeOpenFileDialog()
+        Dim readRegValue As String
+        readRegValue = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\SOFTWARE\Translation Tools\Field Reader Document Creator", "Last Source", Nothing)
+        If String.IsNullOrEmpty(readRegValue) Then
+            Me.OpenFileDialog1.InitialDirectory = "H:\Trans\Export"
+        Else
+            Me.OpenFileDialog1.InitialDirectory = readRegValue
+        End If
+        Me.OpenFileDialog1.Filter = "All Files *.* | *.*"
+        ' Allow the user to select multiple images.
+        Me.OpenFileDialog1.Multiselect = True
+        Me.OpenFileDialog1.Title = "Document Browser"
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        InitializeOpenFileDialog()
+        Label3.Text = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\SOFTWARE\Translation Tools\Field Reader Document Creator", "Last Destination", Nothing)
+    End Sub
+
+    Private Sub ListBox1_DragEnter(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles ListBox1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.All
+        End If
+    End Sub
+
+    Private Sub ListBox1_DragDrop(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles ListBox1.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim MyFiles() As String
+            Dim i As Integer
+
+            MyFiles = e.Data.GetData(DataFormats.FileDrop)
+            For i = 0 To MyFiles.Length - 1
+                ListBox1.Items.Add(MyFiles(i))
+            Next
+            Dim path As String = System.IO.Path.GetDirectoryName(ListBox1.Items.Item(ListBox1.Items.Count - 1))
+            My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Translation Tools\Field Reader Document Creator", True)
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\SOFTWARE\Translation Tools\Field Reader Document Creator", "Last Source", path)
+            If Not String.IsNullOrEmpty(Label3.Text) Then
+                Button4.Enabled = True
+            End If
+            Button2.Enabled = True
+            Button6.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dr As DialogResult = Me.OpenFileDialog1.ShowDialog()
+        If (dr = System.Windows.Forms.DialogResult.OK) Then
+            Button2.Enabled = True
+            Button6.Enabled = True
+            ' Read the files
+            Dim filename As String
+            For Each file In OpenFileDialog1.FileNames
+                filename = System.IO.Path.GetFileName(file)
+                ListBox1.Items.Add(file)
+            Next file
+            Dim path As String = System.IO.Path.GetDirectoryName(OpenFileDialog1.FileNames(OpenFileDialog1.FileNames.Count - 1))
+            Dim regValue As String
+            regValue = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\SOFTWARE\Translation Tools\Field Reader Document Creator", "Last Source", Nothing)
+            If String.IsNullOrEmpty(regValue) Then
+                My.Computer.Registry.SetValue("HKEY_CURRENT_USER\SOFTWARE\Translation Tools\Field Reader Document Creator", "Last Source", path)
+            Else
+                My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Translation Tools\Field Reader Document Creator", True)
+                My.Computer.Registry.SetValue("HKEY_CURRENT_USER\SOFTWARE\Translation Tools\Field Reader Document Creator", "Last Source", path)
+            End If
+            If Not String.IsNullOrEmpty(Label3.Text) Then
+                Button4.Enabled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' TODO: Add ability to remove multiple items
+        ' For i = 0 To ListBox1.SelectedItems.Count - 1
+        ' ListBox1.Items.Remove(ListBox1.SelectedItems(i))
+        ' Next
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+        If ListBox1.Items.Count = 0 Then
+            Button2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim initialDirectory As String = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\SOFTWARE\Translation Tools\Field Reader Document Creator", "Last Destination", Nothing)
+        If String.IsNullOrEmpty(initialDirectory) Then
+        End If
+        Using obj As New OpenFileDialog
+            obj.Filter = "foldersOnly|*.none"
+            obj.CheckFileExists = False
+            obj.CheckPathExists = False
+            If String.IsNullOrEmpty(initialDirectory) Then
+                obj.InitialDirectory = "C:\temp"
+            Else
+                obj.InitialDirectory = initialDirectory
+            End If
+            'obj.CustomPlaces.Add("H:\OIS") ' add your custom location, appears upper left
+            'obj.CustomPlaces.Add("H:\Permits") ' add your custom location
+            obj.Title = "Select folder - click Open to select"
+            obj.FileName = "OpenFldrPath"
+            If obj.ShowDialog = Windows.Forms.DialogResult.OK Then
+                Dim dest As String = System.IO.Path.GetDirectoryName(obj.FileName)
+                Label3.Text = dest
+                My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Translation Tools\Field Reader Document Creator", True)
+                My.Computer.Registry.SetValue("HKEY_CURRENT_USER\SOFTWARE\Translation Tools\Field Reader Document Creator", "Last Destination", Label3.Text)
+                Button4.Enabled = True
+            End If
+        End Using
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = ListBox1.Items.Count * 2
+        ProgressBar1.Value = 0
+        Dim files() As String = ListBox1.Items.Cast(Of String).ToArray
+        Dim MEPSFilesList As ArrayList = New ArrayList()
+        Dim destFile As String
+        Dim destMPSFile As String
+        Dim destWDFile As String
+        Dim destPath As String
+        destPath = Label3.Text
+        Dim appWord As Word.Application
+        appWord = New Word.Application
+        Dim wordDoc As Word.Document
+		wordDoc = New Word.Document
+		Dim wordDoc2Save As Word.Document
+		wordDoc2Save = New Word.Document
+		Dim amkCmd As String
+        Dim amkArg As String
+        amkCmd = """C:\Program Files (x86)\Watchtower\MEPS Prepress 1.8.4\AUTOMARK.exe"""
+        'Dim FindObject As Word.Find = Word.Application
+        For Each file In files
+			destFile = System.IO.Path.Combine(Label3.Text, System.IO.Path.GetFileName(file).ToString)
+			System.IO.File.Copy(file, destFile, True)
+			ProgressBar1.Value += 1
+			destMPSFile = Replace(destFile, ".docx", ".mps")
+			'destMPSFile = destFile & ".mps"
+			wordDoc2Save = appWord.Documents.Open(destFile)
+			wordDoc2Save.SaveAs2(FileName:=destMPSFile, FileFormat:=100)
+			wordDoc2Save.Close()
+			MEPSFilesList.Add(destMPSFile)
+			System.IO.File.Delete(destFile)
+			'My.Computer.FileSystem.RenameFile(destFile, System.IO.Path.GetFileName(destMPSFile).ToString)
+			Label4.Text = Int(ProgressBar1.Value * 100 / ProgressBar1.Maximum) & "%"
+		Next file
+		amkArg = """" & destPath & """  G:\SYSTEM\AUTOMARK.UP\FieldReader.NV R I"
+        Dim p As Process = Process.Start(amkCmd, amkArg)
+        p.WaitForInputIdle()
+        p.WaitForExit()
+        For Each mepsFile In MEPSFilesList
+            wordDoc = appWord.Documents.Open(mepsFile)
+            appWord.Selection.WholeStory()
+            With appWord.Selection.ParagraphFormat
+                .SpaceBefore = 6
+                .SpaceBeforeAuto = False
+                .SpaceAfter = 6
+                .SpaceBeforeAuto = False
+            End With
+            With appWord.Selection
+                .LanguageID = Word.WdLanguageID.wdEnglishUS
+                .NoProofing = True
+            End With
+            appWord.Run("ProcessQuestions")
+            appWord.Run("ProcessOther")
+            wordDoc.Sections(1).Footers(1).PageNumbers.Add(1)
+            appWord.ActiveDocument.TrackRevisions = True
+            appWord.ActiveDocument.ShowRevisions = True
+			destWDFile = Replace(mepsFile, ".mps", ".docx")
+			appWord.ActiveDocument.SaveAs2(destWDFile)
+            wordDoc.Close()
+            wordDoc = Nothing
+            System.IO.File.Delete(mepsFile)
+            ProgressBar1.Value += 1
+            Label4.Text = Int(ProgressBar1.Value * 100 / ProgressBar1.Maximum) & "%"
+        Next
+        appWord.Quit()
+        Process.Start(Label3.Text)
+        Label4.Text = "Done!"
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        ListBox1.Items.Clear()
+        Button2.Enabled = False
+        Button4.Enabled = False
+        Button6.Enabled = False
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+End Class
